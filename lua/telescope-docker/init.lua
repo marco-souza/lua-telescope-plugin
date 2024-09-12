df672b2bbe85d20da7579de8f475fcf484253035
Marco Antônio <ma.souza.junior@gmail.com>
2024-09-12 14:48:58 -0300

    fea: create entry maker

--- lua/telescope-docker/init.lua
+++ lua/telescope-docker/init.lua
@@ -7,7 +7,18 @@ local M = {}
 M.show_docker_images = function(opts)
   pickers.new(opts, {
     finder = finders.new_table({
-      "Yes", "No", "Maybe", "4,20:69"
+      results = {
+        { name = "Brazil",    value = { 1, 2, 3, 5, 45 } },
+        { name = "Peru",      value = { 1, 2, 3, 5, 45 } },
+        { name = "Argentina", value = { 1, 2, 3, 5, 45 } },
+      },
+      entry_maker = function(entry)
+        return {
+          -- value = entry,
+          display = entry.name, -- display key
+          ordinal = entry.name, -- sorting key
+        }
+      end
     }),
     sorter = config.generic_sorter(opts),
   }):find()
