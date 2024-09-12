2f9fe81dc3920546224f9b728bc99637bd326b18
Marco Antônio <ma.souza.junior@gmail.com>
2024-09-12 14:41:47 -0300

    fea: super simple telescope plugin

--- lua/telescope-docker/init.lua
+++ lua/telescope-docker/init.lua
@@ -1,7 +1,19 @@
+local pickers = require('telescope.pickers')
+local finders = require('telescope.finders')
+local config = require('telescope.config').values
+
 local M = {}
 
-M.show_docker_images = function()
+M.show_docker_images = function(opts)
+  pickers.new(opts, {
+    finder = finders.new_table({
+      "Yes", "No", "Maybe", "4,20:69"
+    }),
+    sorter = config.generic_sorter(opts),
+  }):find()
   print("Showing docker images")
 end
+
+M.show_docker_images()
 
 return M
